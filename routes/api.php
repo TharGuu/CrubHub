a594ea033458a0f3138c3b6db89f8e32284e7c89
TharGuu <tharguu000187@gmail.com>
2025-08-13 11:53:27 +0700

    Add API CommentController and update routes for Postman/Flutter usage

--- routes/api.php
+++ routes/api.php
@@ -4,24 +4,31 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController as ApiPostController;
 use App\Http\Controllers\Api\CommentController as ApiCommentController;
+use App\Http\Controllers\Api\StreamCommentController; // <-- make sure this exists
 
+// Public routes
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class,'logout']);
 
+    // Stream comments
     Route::get('/stream/comments',  [StreamCommentController::class,'index']);
     Route::post('/stream/comments', [StreamCommentController::class,'store']);
 
+    // Posts
     Route::get('/posts', [ApiPostController::class,'index']);
     Route::post('/posts', [ApiPostController::class,'store']);
     Route::get('/posts/{post}', [ApiPostController::class,'show']);
     Route::put('/posts/{post}', [ApiPostController::class,'update']);
     Route::delete('/posts/{post}', [ApiPostController::class,'destroy']);
 
+    // Comments on posts
     Route::post('/posts/{post}/comments', [ApiCommentController::class,'store']);
     Route::get('/posts/{post}/comments', [ApiCommentController::class,'index']);
 });
 
-Route::get('/ping', fn() => 'pong');
+// Simple test endpoint
+Route::get('/ping', fn() => response()->json(['message' => 'pong']));
